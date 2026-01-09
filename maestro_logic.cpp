058ee7508903d979f3bd6bb410d6b34aa9adcfb9
mateimalin <mateimalin08@gmail.com>
2026-01-10 01:26:10 +0200

    bafta

--- maestro_logic.cpp
+++ maestro_logic.cpp
@@ -3,13 +3,16 @@
 
 Value::Value() : type("silenzio"), iVal(0), fVal(0.0), bVal(false) {}
 
-Symbol::Symbol(std::string _name, std::string _type, std::string _kind) : name(_name), type(_type), kind(_kind) {
-    if (type == "basso") { value.type = "basso"; value.iVal = 0; }
-    else if (type == "soprano") { value.type = "soprano"; value.fVal = 0.0; }
-    else if (type == "verita") { value.type = "verita"; value.bVal = false; }
+Symbol::Symbol(std::string _name, std::string _type, std::string _kind) {
+    name = _name; type = _type; kind = _kind;
+    if (type == "basso") value.type = "basso", value.iVal = 0;
+    else if (type == "soprano") value.type = "soprano", value.fVal = 0.0;
+    else if (type == "verita") value.type = "verita", value.bVal = false;
 }
 
-SymbolTable::SymbolTable(std::string _scope, SymbolTable* _parent) : scope(_scope), parent(_parent) {}
+SymbolTable::SymbolTable(std::string _scope, SymbolTable* _parent)
+    : scope(_scope), parent(_parent), lastAddedFunction(nullptr) {
+}
 
 bool SymbolTable::addSymbol(std::string _name, std::string _type, std::string _kind) {
     if (symbols.find(_name) != symbols.end()) return false;
@@ -31,7 +34,9 @@ Symbol* SymbolTable::findSymbol(std::string name) {
 
 void SymbolTable::printTable(std::ofstream& fout) {
     fout << "Symbol Table: " << scope << "\n____________________________________________\n";
-    for (auto const& [key, val] : symbols) fout << val.name << "\t" << val.type << "\t" << val.kind << "\n";
+    for (auto const& [key, val] : symbols) {
+        fout << val.name << "\t" << val.type << "\t" << val.kind << "\n";
+    }
     fout << "\n";
 }
 
@@ -39,33 +44,57 @@ ASTNode::ASTNode(NodeType t) : nodeType(t), left(nullptr), right(nullptr) {}
 
 Value ASTNode::evaluate(SymbolTable* currentScope) {
     Value res;
+
     switch (nodeType) {
-    case NODE_LITERAL: return literalValue;
+    case NODE_LITERAL: {
+        return literalValue; 
+    }
+
     case NODE_ID: {
         Symbol* symbol = currentScope->findSymbol(idName);
-        if (!symbol) { std::cerr << "Semantic Error: " << idName << " undefined\n"; exit(1); }
+        if (symbol == nullptr) {
+            std::cerr << "[EROARE SEMANTICA] Variabila " + idName + " nu este definita!\n";
+            exit(1);
+        }
         return symbol->value;
     }
+
     case NODE_ASSIGN: {
         Value val = right->evaluate(currentScope);
         Symbol* symbol = currentScope->findSymbol(idName);
-        if (!symbol) { std::cerr << "Semantic Error: " << idName << " undefined\n"; exit(1); }
-        if (symbol->type != val.type) { std::cerr << "Type Error: mismatch in assignment\n"; exit(1); }
-        symbol->value = val;
+
+        if (symbol == nullptr) {
+            std::cerr << "[EROARE SEMANTICA] Variabila " + idName + " nu este definita!\n";
+            exit(1);
+        }
+
+        if (symbol->type != val.type) {
+            std::cerr << "[EROARE SEMANTICA] Nu poti atribui " + val.type + " la " + symbol->type << "\n";
+            exit(1);
+        }
+
+        symbol->value = val; //  SymbolTable
         return val;
     }
+
     case NODE_PRINT: {
-        Value val = left->evaluate(currentScope);
+        Value val = left->evaluate(currentScope); // evaluate left tree
         if (val.type == "basso") std::cout << val.iVal << "\n";
         else if (val.type == "soprano") std::cout << val.fVal << "\n";
         else if (val.type == "libretto") std::cout << val.sVal << "\n";
         else if (val.type == "verita") std::cout << (val.bVal ? "vero" : "falso") << "\n";
         return val;
     }
+
     case NODE_OP: {
         Value l = left->evaluate(currentScope);
         Value r = right->evaluate(currentScope);
-        if (l.type != r.type) { std::cerr << "Semantic Error: Operands must match type\n"; exit(1); }
+
+        if (l.type != r.type) {
+            std::cerr << "[EROARE SEMANTICA] Tipuri incompatibile pentru " + op << "\n";
+            exit(1);
+        }
+
         res.type = l.type;
         if (op == "+") {
             if (l.type == "basso") res.iVal = l.iVal + r.iVal;
@@ -75,20 +104,13 @@ Value ASTNode::evaluate(SymbolTable* currentScope) {
             if (l.type == "basso") res.iVal = l.iVal - r.iVal;
             else res.fVal = l.fVal - r.fVal;
         }
-        else if (op == "*") {
-            if (l.type == "basso") res.iVal = l.iVal * r.iVal;
-            else res.fVal = l.fVal * r.fVal;
-        }
         return res;
     }
+
     case NODE_OTHER: {
         Symbol* function = currentScope->findSymbol(idName);
-        if (!function) { std::cerr << "Semantic Error: Function " << idName << " undefined\n"; exit(1); }
-        if (function->paramTypes.size() != arguments.size()) { std::cerr << "Semantic Error: Arg count mismatch\n"; exit(1); }
-        for (size_t i = 0; i < arguments.size(); i++) {
-            Value argVal = arguments[i]->evaluate(currentScope);
-            if (function->paramTypes[i] != argVal.type) { std::cerr << "Semantic Error: Arg type mismatch\n"; exit(1); }
-        }
+        if (!function) { std::cerr << "Functie inexistenta!\n"; exit(1); }
+        if (function->paramTypes.size() != arguments.size()) { std::cerr << "Eroare argumente!\n"; exit(1); }
         res.type = function->type;
         return res;
     }
--- maestro_logic.h
+++ maestro_logic.h
@@ -14,7 +14,8 @@ public:
     float fVal;
     std::string sVal;
     bool bVal;
-    Value(); // Constructor
+
+    Value();
 };
 
 class SymbolTable;
@@ -23,8 +24,11 @@ class Symbol {
 public:
     std::string name, type, kind;
     std::vector<std::string> paramTypes;
+    SymbolTable* nestedScope;
+
     Value value;
-    Symbol(std::string _name, std::string _type, std::string _kind); // Constructor
+
+    Symbol(std::string _name, std::string _type, std::string _kind);
 };
 
 class SymbolTable {
@@ -32,13 +36,14 @@ public:
     std::string scope;
     SymbolTable* parent;
     std::map<std::string, Symbol> symbols;
-    Symbol* lastAddedFunction = nullptr;
+    Symbol* lastAddedFunction;
 
     SymbolTable(std::string _scope, SymbolTable* _parent = nullptr);
-    bool addSymbol(std::string _name, std::string _type, std::string _kind); //
-    void addParamToLastFunction(std::string paramType); //
-    Symbol* findSymbol(std::string name); //
-    void printTable(std::ofstream& fout); //
+
+    bool addSymbol(std::string _name, std::string _type, std::string _kind);
+    void addParamToLastFunction(std::string paramType);
+    Symbol* findSymbol(std::string name);
+    void printTable(std::ofstream& fout);
 };
 
 enum NodeType { NODE_LITERAL, NODE_ID, NODE_OP, NODE_ASSIGN, NODE_PRINT, NODE_OTHER };
@@ -49,11 +54,13 @@ public:
     std::string op;
     Value literalValue;
     std::string idName;
+    std::string exprType;
     ASTNode* left, * right;
+
     std::vector<ASTNode*> arguments;
 
     ASTNode(NodeType t);
-    Value evaluate(SymbolTable* scope); //
+    Value evaluate(SymbolTable* currentScope);
 };
 
 #endif
